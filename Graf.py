b585dad824969381dadbe2d4782b0e05def316ce
milosz <milosz@deb.home>
2020-04-18 16:55:06 +0100

    Dodano algorytm najmniejszej krawędzi.

--- Graf.py
+++ Graf.py
@@ -2,12 +2,18 @@ import numpy as np
 
 class Graf():
     """Reprezentacja grafu nieskierowanego w postaci macierzy sasiedztwa."""
-    G = None
-    n = 0
-
     def __init__(self, n):
         self.G = np.zeros((n,n))
         self.n = n
+
+    def TEST(self):
+        self.G = np.array ([[0,91,56,99,70,87,70],
+                            [91,0,38,83,70,47,3],
+                            [56,38 ,0 ,51,8,95,16],
+                            [99,83,51,0,24,55,73],
+                            [70,70 ,8,24,0,50,19],
+                            [87,47,95,55 ,50,0,15],
+                            [70,3,16,73,19,15,0],])
 
     def __repr__(self):
         return self.G
@@ -23,21 +29,21 @@ class Graf():
             yield i
 
     def addE(self, u, v, w):
-        """Dodaje krawedz z wierzcholka u do v z waga w."""
+        """Dodaje krawędź z wierzchołka u do v z wagą w."""
         if u != v:
-            self.G[u-1][v-1] = w
-            self.G[v-1][u-1] = w
+            self.G[u][v] = w
+            self.G[v][u] = w
 
     def removeE(self, u, v):
-        """Usuwa krawedz z u do v"""
+        """Usuwa krawędź z u do v"""
         self.addE(u,v, 0)
 
     def getWeight(self, u, v):
-        """Zwraca wage krawedzi z wierzcholka u do v."""
-        return self.G[u-1,v-1]
+        """Zwraca wagę krawędzi z wierzchołka u do v."""
+        return self.G[u,v]
 
     def getRouteWeight(self, route):
-        """Liczy sume wag krawedzi na sciezce"""
+        """Liczy sumę wag krawędzi na scieżce."""
         routeWeight =[]
 
         for r in range(len(route)-1):
@@ -45,21 +51,154 @@ class Graf():
 
         return sum(routeWeight)
 
-
     def full_randomize(self):
-        """Tworzy pelny graf z n wierzcholkow z losowymi wagami"""
+        """Tworzy pełny graf n wierzchołkow z losowymi wagami."""
         self.G = np.random.randint(1,100, self.G.shape)
-        self.G = self.G + self.G.T  #tworzy macierz symetryczna
+
+        #tworzy macierz symetryczna
+        for i in range(self.n):
+            for j in range(len(self.G[i][:i])):
+                self.G[i][j] = self.G[j][i]
+
         np.fill_diagonal(self.G, 0)
         return self.G
 
 
+class Graf_adjlist():
+    """Reprezentacja grafu nieskierowanego w postaci listy sąsiedztwa."""
+
+    def __init__(self, vertices):
+        self.v = vertices
+        self.G = [None] * self.v
+
+    def add_edge(self, src, dest):
+        node = Node(dest)
+        node.next = self.G[src]
+        self.G[src] = node
+
+        node = Node(src)
+        node.next = self.G[dest]
+        self.G[dest] = node
+
+    def print_graph(self):
+        for i in range(self.v):
+            print("Lista sąsiedztwa wierzchołka {}:\nhead".format(i), end="")
+            temp = self.G[i]
+            while temp:
+                print(" -> {}".format(temp.key), end="")
+                temp = temp.next
+            print(" \n")
+
+    def size(self, node):
+        """Funkcja zwracająca ilość krawędzi wychodzących z wierzchołka."""
+        size = 0
+        temp = self.G[node]
+        while temp:
+            temp = temp.next
+            size += 1
+        return size
+
+    def is_not_third(self, u, v):
+        """Funkcja która sprawdza, czy dodanie krawędzi spowoduje powstanie wierzchołka,
+        z którego wychodzą trzy krawędzie."""
+        if self.size(u) < 2 and self.size(v) < 2:
+            return True
+
+    def has_cycle(self, u, v):
+        """Funkcja wykrywająca cykl w grafie, gdy dodawana jest krawędź (u,v)."""
+
+        #cykl może wystąpić tylko wtedy, gdy każdy z wierzchołków u i v posiada istniejącą krawędź
+        if self.G[u] == None or self.G[v] == None:
+            return False
+
+        current = self.G[u]
+        visited = [u]
+
+
+        while current.key != self.G[v].key:
+
+            #pętla while przechodząca po danej liście sąsiedctwa wierzchołka grafu
+            while self.G[current.key].next != None:
+                if self.G[current.key].key not in visited:
+                    visited.append(current.key)
+                    break
+                else:
+                    current = self.G[current.key].next
+
+            #warunek istnienia cyklu
+            if current.key == self.G[v].key:
+                return True
+
+            #warunek obsługujący przechodzenie po wierzchołkach ścieżki grafu
+            if self.G[current.key].key not in visited:
+                current = self.G[current.key]
+                visited.append(current.key)
+            else:
+                return False
+
+
+    def get_path(self):
+        """Funkcja przechodząca przez cały graf zwracająca ścieżkę."""
+        current = self.G[0]
+        visited = [0]
+
+        for n in range(self.v):
+            while current:
+                if current.key not in visited:
+                    visited.append(current.key)
+                    break
+                elif current.next != None:
+                    current = current.next
+                else:
+                    break
+            current = self.G[current.key]
+
+        visited.append(visited[0])
+        return visited
+
+
+class Node():
+    """Wierzchołek grafu."""
+    def __init__(self, key, next = None):
+        self.next = next
+        self.key = key
+
+    def __repr__(self):
+        return str(self.key)
+
+
+class Edge():
+    """Krawędź grafu."""
+    def __init__(self, u, v, w):
+        self.u = Node(u, v)
+        self.v = Node(v, u)
+        self.w = w
+
+    def __str__(self):
+        return "{},{},{}".format(self.u.key,self.v.key,self.w)
+
+    def __repr__(self):
+        return str((self.u.key, self.v.key,self.w))
+
+    def get_vertices(self):
+        yield self.u.key
+        yield self.v.key
+
+
 if __name__ == "__main__":
-    G = Graf(4)
-    G.addE(1,2,2)
+    G = Graf(7)
+    G.addE(0,2,6)
     G.addE(1,4,5)
     G.addE(3,3,4)
     G.removeE(1,4)
     print(G)
-    print(G.getWeight(2,1))
     print(G.full_randomize())
+
+    lst = []
+    e = Edge(1,2,20)
+    lst.append(e)
+    print(lst)
+
+    node1 = Node(1)
+    node2 = Node(2)
+    edge1 = Edge(node1,node2, 10)
--- main.py
+++ main.py
@@ -9,25 +9,25 @@ def brute_force(G):
     """Rozwiazuje TSP metoda sprawdzenia wszyskich mozliwosci"""
     bestRoute = None
     bestRouteWeight = 0
-    routes = list(permutations(range(1, G.n+1)))    #znajduje wszyskie n permutacji wierzcholków
+    routes = list(permutations(range(0, G.n)))    #znajduje wszyskie n permutacji wierzcholków
     routes = [ list(n) for n in routes]
 
-    for n in routes:    #łaczy ścieżki w cykle
+    for n in routes:    #łączy ścieżki w cykle
         n.append(n[0])
 
     for r in routes:
-        routeWeight = G.getRouteWeight(r)
-        if routeWeight < bestRouteWeight or bestRouteWeight == 0:
-            bestRouteWeight = routeWeight
+        temp_weight = G.getRouteWeight(r)
+        if temp_weight < bestRouteWeight or bestRouteWeight == 0:
+            bestRouteWeight = temp_weight
             bestRoute = r
 
     return bestRoute, bestRouteWeight
 
-def shift_right(list):
-    return list[1:] + [list[0]]
+
+def shift_right(lst):
+    return lst[1:] + [lst[0]]
 
 
-#co jeśli dwie krawedzie mają tą sama wage?
 def nearest_neighbour(G, current):
     bestRoute = [0 for n in range(G.n)]
     visited = [current]
@@ -42,7 +42,7 @@ def nearest_neighbour(G, current):
                 minimum = (G[current][j], j)
 
         current = minimum[1]
-        bestRoute[i] = (visited[-1] +1, current +1)
+        bestRoute[i] = (visited[-1], current)
         visited.append(minimum[1])
 
     bestRoute = [n[0] for n in bestRoute]
@@ -54,39 +54,44 @@ def nearest_neighbour(G, current):
 
     return bestRoute
 
+
 def smallest_edge(G):
     queue = []
     bestRoute = []
 
     #tworzy listę krawędzi wraz z ich wagami
+    #krawędzie nie powtarzaja się tj. nie wystepuje krawedz (a,b,w) i (b,a,w)
     for n in range(0, G.n -1):
-        min = [n+1, 0, G[n][n+1]]
+        minimum = [n, G[n][n], G[n][n+1]]     #[wiersz, kolumna, waga]
         for j, k in enumerate( G[n][n+1:], n):
-            min[1] = j+2
-            min[2] = k
-            queue.append(tuple(min))
+            minimum[1] = j+1
+            minimum[2] = k
+            queue.append(Graf.Edge(*minimum))
 
     #sortowanie kolejki rosnaco według wag
-    queue = sorted(queue, key = lambda x: x[2])
+    queue.sort(key = lambda x: x.w)
 
-    #sprawdza czy dołączenie tej krawędzi do rozwiązania nie spowoduje utworzenia cyklu i tworzy rozwiazanie
-    while len(bestRoute) != G.n:
+    solution = Graf.Graf_adjlist(G.n )
+
+    edge_count = 0
+    for i in range(len(queue)):
         edge = queue.pop(0)
-        if edge[0] not in bestRoute:
-            bestRoute.append(edge[0])
-        if edge[1] not in bestRoute:
-            bestRoute.append(edge[1])
 
-    #przesuwa liste rozwiazania tak aby wierzcholek 1 znalazł sie na jej poczatku
-    while bestRoute[0] != 1:
-        bestRoute = shift_right(bestRoute)
+        #dodaje krawedz w ostatnej iteracji algorytmu
+        if edge_count == G.n-1 and solution.is_not_third(edge.u.key, edge.v.key):
+            solution.add_edge(edge.u.key, edge.v.key)
 
-    bestRoute.append(bestRoute[0])
+        #dodaje krawędź, gdy nie powstanie wierzchołek, z którego wychodzą trzy krawędzie oraz nie powstanie cykl
+        elif solution.is_not_third(edge.u.key, edge.v.key) and solution.has_cycle(edge.u.key, edge.v.key) == False:
+            solution.add_edge(edge.u.key, edge.v.key)
+            edge_count += 1
 
-    return bestRoute
+    #solution.print_graph()
+    return solution.get_path()
+
 
 def RNN(G):
-    """Powtarzalny algorytm najblizszego sasiada"""
+    """Powtarzalny algorytm najbliższego sąsiada."""
     bestRoute = None
     bestRouteWeight = 0
 
@@ -94,7 +99,6 @@ def RNN(G):
         temp_route = nearest_neighbour(G, n)
         temp_weight = G.getRouteWeight(temp_route)
 
-        #print(temp_route)
         if temp_weight < bestRouteWeight or bestRouteWeight == 0:
             bestRouteWeight = temp_weight
             bestRoute = temp_route
@@ -104,26 +108,29 @@ def RNN(G):
 
 G = Graf.Graf(7)
 G.full_randomize()
+#G.TEST()
 
 print("Najkrótsza scieżka problemu TSP.")
+print(G)
+
 start_time = time.time()
 bR_BF, bRW_BF = brute_force(G)
 final_time = time.time() - start_time
-print("Algorytm typu brute force: {}. Łączna waga krawędzi: {}. \nCzas wykonania: {}.".format(bR_BF, bRW_BF, final_time))
+print("\nAlgorytm typu brute force: {}. Łączna waga krawędzi: {}. \nCzas wykonania: {}.".format(bR_BF, bRW_BF, final_time))
 
 start_time = time.time()
 bR_NN = nearest_neighbour(G, 0)
 final_time = time.time() - start_time
 bRW_NN = G.getRouteWeight(bR_NN)
-print("Algorytm najbliższego sąsiada: {}. Łączna waga krawędzi: {}. \nCzas wykonania: {}.".format(bR_NN, bRW_NN, final_time))
+print("\nAlgorytm najbliższego sąsiada: {}. Łączna waga krawędzi: {}. \nCzas wykonania: {}.".format(bR_NN, bRW_NN, final_time))
 
 start_time =   time.time()
 bR_SE = smallest_edge(G)
 final_time = time.time() - start_time
 bRW_SE = G.getRouteWeight(bR_SE)
-print("Algorytm najmniejszej krawędzi: {}. Łączna waga krawędzi: {}. \nCzas wykonania: {}".format(bR_SE, bRW_SE, final_time))
+print("\nAlgorytm najmniejszej krawędzi: {}. Łączna waga krawędzi: {}. \nCzas wykonania: {}".format(bR_SE, bRW_SE, final_time))
 
 start_time =   time.time()
 bR_RNN, bRW_RNN = RNN(G)
 final_time = time.time() - start_time
-print("Powtarzalny algorytm najbliższego sąsiada: {}. Łączna waga krawędzi: {}. \nCzas wykonania: {}.".format(bR_RNN, bRW_RNN, final_time))
+print("\nPowtarzalny algorytm najbliższego sąsiada: {}. Łączna waga krawędzi: {}. \nCzas wykonania: {}.".format(bR_RNN, bRW_RNN, final_time))
